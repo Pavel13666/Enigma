aa1119004824ce55f226d3891880741cec0829ed
Pavel <paha13666@gmail.com>
2024-02-23 17:50:12 +0200

    Setup

---
+++ app/scripts/pages/homePage.ts
@@ -0,0 +1,31 @@
+import 'styles/index.sass';
+
+import Section, { SectionCtor } from '../core/section';
+import CommonPage from './commonPage';
+import HeroSection from 'app/sections/HeroSection';
+import TabSection from 'app/sections/TabsSections';
+
+export default class HomePage extends CommonPage {
+    _sectionTypes: SectionCtor[] = [
+        Section,
+        Section,
+        Section,
+        Section,
+        Section,
+        Section,
+        Section,
+        Section,
+        Section,
+        Section
+    ];
+
+    async setupPageAsync() {
+        super.setupPageAsync();
+    }
+
+    get sectionTypes() {
+        return this._sectionTypes;
+    }
+
+}
+HomePage.RunPage(HomePage);
